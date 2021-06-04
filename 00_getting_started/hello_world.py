c5aedc663368f7f5158c306f106ed4c13b3917ae
cslocum <christopher.slocum@colostate.edu>
2021-06-04 04:55:55 -0600

    Getting started files

---
+++ 00_getting_started/hello_world.py
@@ -0,0 +1,10 @@
+"""
+This is a docstring (documentation string) for the
+program hello_world.py.
+
+In this script prints the string "Hello, World!"
+"""
+
+# This is a comment
+# The line of code below outputs the string "Hello, World!"
+print("Hello, World!")
