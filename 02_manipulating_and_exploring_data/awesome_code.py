c15926da4eb35280bd9ec2bf59b60df25974f747
cslocum <christopher.slocum@colostate.edu>
2022-06-08 07:08:28 -0600

    fixed variable naming issue

--- 02_manipulating_and_exploring_data/awesome_code.py
+++ 02_manipulating_and_exploring_data/awesome_code.py
@@ -6,19 +6,19 @@ fixes longitude values.
 def awesome_longitude_fix(lon_array):
     """
     Fixes longitude values to be between 0 & 360
-    
+
     Parameters
     ----------
     lon_array : array-like
         array of longitude values that needs to be fixed
-    
+
     Return
     ------
     lon_array : array-like
         longitude array between 0 & 360
     """
-    condition_less = input_lon_array < 0
-    condition_greater = input_lon_array >= 360
-    input_lon_array[condition_less] += 180
-    input_lon_array[condition_greater] -= 180
+    condition_less = lon_array < 0
+    condition_greater = lon_array >= 360
+    lon_array[condition_less] += 180
+    lon_array[condition_greater] -= 180
     return lon_array
