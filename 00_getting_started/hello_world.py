e52bbbeed9bec421be6dfacfebe95d531f805188
cslocum <christopher.slocum@colostate.edu>
2023-06-06 07:50:30 -0600

    added link to NumPy docstring format style guide

--- 00_getting_started/hello_world.py
+++ 00_getting_started/hello_world.py
@@ -2,6 +2,10 @@
 This is a docstring (documentation string) for the
 program hello_world.py.
 
+For more info on docstring formats, see the NumPy docstring
+style guide (commonly used in sciencific packages).
+https://numpydoc.readthedocs.io/en/latest/format.html
+
 In this script prints the string "Hello, World!"
 """
 
